3684f20b6e17266a1e32d6b486aec09beffeb245
rajatsah95 <rajatsah95@gmail.com>
2024-11-22 17:12:00 +0530

    healthcare added

--- configs/mongodb.js
+++ configs/mongodb.js
@@ -1,4 +1,4 @@
 let mongoose=require("mongoose")
-let connection=mongoose.connect("mongodb://localhost:27017")
+let connection=mongoose.connect("mongodb://localhost:27017/imdb")
 
 module.exports={connection}
